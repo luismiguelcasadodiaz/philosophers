364a8a496aaa4e5ce179ce5391c31ac3428c5a69
Luis Miguel Casado Diaz <luismiguelcasadodiaz@gmail.com>
2024-07-23 14:15:41 +0200

    thread error

--- test/Butenhof/thread_error.c
+++ test/Butenhof/thread_error.c
@@ -1,29 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   thread_error.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: luicasad <luicasad@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/21 11:09:08 by luicasad          #+#    #+#             */
+/*   Updated: 2024/07/21 11:13:49 by luicasad         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 /*
  * thread_error.c
  *
  * Demonstrate detection of errors from a typical POSIX 1003.1c-1995
  * function, pthread_join.
  */
+/*
+ * Attempt to join with an uninitialized thread ID. On most
+ * implementations, this will return an ESRCH error code. If
+ * the local (and uninitialized) pthread_t happens to be a valid
+ * thread ID, it is almost certainly that of the initial thread,
+ * which is running main(). In that case, your Pthreads
+ * implementation may either return EDEADLK (self-deadlock),
+ * or it may hang. If it hangs, quit and try again.
+ */
 #include <pthread.h>
 #include <stdio.h>
 #include <errno.h>
+#include <string.h>
 
-int main (int argc, char *argv[])
+int	main(void)
 {
-    pthread_t thread;
-    int status;
+	pthread_t	thread;
+	int			status;
 
-    /*
-     * Attempt to join with an uninitialized thread ID. On most
-     * implementations, this will return an ESRCH error code. If
-     * the local (and uninitialized) pthread_t happens to be a valid
-     * thread ID, it is almost certainly that of the initial thread,
-     * which is running main(). In that case, your Pthreads
-     * implementation may either return EDEADLK (self-deadlock),
-     * or it may hang. If it hangs, quit and try again.
-     */
-    status = pthread_join (thread, NULL);
-    if (status != 0)
-        fprintf (stderr, "error %d: %s\n", status, strerror (status));
-    return status;
+	status = pthread_join (thread, NULL);
+	if (status != 0)
+		fprintf (stderr, "error %d: %s\n", status, strerror (status));
+	return (status);
 }
