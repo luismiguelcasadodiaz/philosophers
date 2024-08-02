65f5dafe0d4f8d1ac8305ecd6e792ae83afd273a
Luis Miguel Casado Díaz <luismiguelcasadodiaz@gmail.com>
2024-08-02 13:13:27 +0200

    If thread creation fails, free threads argument before exit the funciton . that avoids a memory leak.

--- philo/philo_creation.c
+++ philo/philo_creation.c
@@ -6,7 +6,7 @@
 /*   By: luicasad <luicasad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/25 17:42:43 by luicasad          #+#    #+#             */
-/*   Updated: 2024/07/29 18:37:37 by luicasad         ###   ########.fr       */
+/*   Updated: 2024/08/02 12:44:59 by luicasad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,11 @@ static int	philo_creat_one(t_moni *moni, int i)
 	while (i <= moni->num_phi)
 	{
 		arg = t_moni_copy_set(i, moni);
-		if (my_th_create(arg->thread_ids[i], philo_thread, arg))
+		if (i == 3 || my_th_create(arg->thread_ids[i], philo_thread, arg))
+		{
+			t_moni_free(arg, PART);
 			return (1);
+		}
 		i = i + 2;
 	}
 	return (0);
@@ -28,15 +31,19 @@ static int	philo_creat_one(t_moni *moni, int i)
 
 int	philo_create(t_moni *moni)
 {
-	int	result;
-
-	result = 0;
 	my_mutex_lock(moni->forks[(moni->num_phi + INITTIME)]);
 	if (philo_creat_one(moni, 1))
-		result = 1;
+	{	//result = 1;
+		my_mutex_unlock(moni->forks[(moni->num_phi + INITTIME)]);
+		return (1);
+	}
 	else if (philo_creat_one(moni, 2))
-		result = 1;
+	{
+		//result = 1;
+		my_mutex_unlock(moni->forks[(moni->num_phi + INITTIME)]);
+		return (1);
+	}
 	*moni->sim_init_ms = my_now_ms();
 	my_mutex_unlock(moni->forks[(moni->num_phi + INITTIME)]);
-	return (result);
+	return (0);
 }
